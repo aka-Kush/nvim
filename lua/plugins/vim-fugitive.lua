cc2d69418b43b15c312ce02eb1fdd8d253471512
Shivansh Kush <kushivansh27@gmail.com>
2025-09-13 17:48:52 +0530

    update

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,6 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>gs', ':Git status<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
   end
 }
