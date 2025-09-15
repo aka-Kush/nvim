97b6025bcec0cd3337c0ca2fb3bfa3a2da7a93aa
Shivansh Kush <kushivansh27@gmail.com>
2025-09-15 23:52:33 +0530

    update

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,20 @@
 return {
-	"numToStr/Comment.nvim",
-	config = function()
-		require("Comment").setup()
-	end,
+    "numToStr/Comment.nvim",
+    config = function()
+        require("Comment").setup({
+            toggler = {
+                line = "gcc", -- line comment toggle
+                block = "gbc", -- block comment toggle
+            },
+            opleader = {
+                line = "gc", -- line comment operator
+                block = "gb", -- block comment operator
+            },
+            extra = {
+                above = "gcO", -- add comment line above
+                below = "gco", -- add comment line below
+                eol = "gcA", -- add comment at end of line
+            },
+        })
+    end,
 }
